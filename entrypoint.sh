e7efa3e725319c0a9cb8713c64d5cf2f85b34235
faro <faruk.rahmet@etiya.com>
2020-06-18 09:13:52 +0300

    worker count changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py migrate
 python manage.py fill_users_cache
 
 /usr/local/bin/gunicorn leaderboard.wsgi:application \
-    --workers 4 \
+    --workers 9 \
     --bind :$APPLICATION_PORT \
     -k gevent
     --capture-output --preload
